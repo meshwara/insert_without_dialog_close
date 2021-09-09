4d1eaf0afedc571cc8458af74d0d3ea11368f467
D. Prameswara <meshwara@gmail.com>
2021-09-09 14:27:02 +0700

    Add dialog

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,43 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row justify="center">
+                    <v-col xl="7">
+                        <v-card>
+                            <v-toolbar>
+                                <v-toolbar-items>
+                                    <v-btn text @click="addData">Add</v-btn>
+                                </v-toolbar-items>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-list-item> </v-list-item>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+        <Dialog ref="dlg" />
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import Dialog from "./components/Dialog.vue";
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    name: "App",
+    components: {
+        Dialog,
+    },
+    data: () => ({
+        items: [],
+    }),
+    methods: {
+        addData() {
+            this.$refs.dlg.open().then(() => {
+                //
+            });
+        },
+    },
 };
 </script>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-dialog v-model="dialog" width="600">        
+        <v-card>
+            <v-toolbar>
+                <v-toolbar-title>Tambah data</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+                <v-text-field label="Nama"></v-text-field>
+                <v-textarea label="Alamat"></v-textarea>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn>Simpan</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nama: "",
+            alamat: "",
+
+            resolve: null,
+            reject: null,
+            dialog: false,
+        };
+    },
+    methods: {
+        open() {
+            return new Promise((resolve, reject) => {
+                this.resolve = resolve;
+                this.reject = reject;
+
+                this.dialog = true;
+            });
+        },
+    },
+};
+</script>
+
+<style></style>
